43fc952ce0364dfd560a4996f5d1ad84b0d45f2b
Xwilarg <xwilarg@yahoo.fr>
2018-07-03 18:04:59 +0200

    Translate dialogs to french

--- Orthesia/Sentences.cs
+++ Orthesia/Sentences.cs
@@ -6,14 +6,14 @@ namespace Orthesia
     {
         public readonly static ulong myId = Program.p.client.CurrentUser.Id;
 
-        public readonly static string hiStr = "Hi";
+        public readonly static string hiStr = "Bonjour";
         public readonly static string deleteConfirm = "Êtes-vous sûr de vouloir fermer la requête ?";
-        public readonly static string chanAlreadyExist = "You already have a opened support channel.";
-        public static string chanCreated(string chanName) { return ("The channel " + chanName + " was created for you."); }
-        public readonly static string wrongChan = "You must be in your support channel to close it.";
-        public readonly static string needWait = "You need to wait at least 10 minutes before opening a new ticket.";
-        public readonly static string help = "**Ticket**: Open a ticket in a private channel" + Environment.NewLine +
-                                            "**Close**: Close the currently opened ticket";
+        public readonly static string chanAlreadyExist = "Vous avez déjà ouvert un canal de support.";
+        public static string chanCreated(string chanName) { return ("Le canal " + chanName + " a été crée pour vous."); }
+        public readonly static string wrongChan = "Vous devez être dans un canal de support pour le fermer.";
+        public readonly static string needWait = "Vous devez attendre 10 minutes entre chaque ticket.";
+        public readonly static string help = "**Ticket**: Ouvre un ticket dans un canal privé." + Environment.NewLine +
+                                            "**Close**: Ferme le ticket couramment ouvert.";
         public readonly static string openRequestPm = "Vous avez crée une requête sur le Discord Orthesia, un membre du staff répondra à votre demande au plus vite.";
         public readonly static string requestClosedPm = "Vous avez fermé la requête avec succès.";
         public readonly static string openRequestChan = "```fix" + Environment.NewLine +
